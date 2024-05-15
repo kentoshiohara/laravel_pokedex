e5e31d007293397a0ec6d19668c259aaf018f507
kentoshiohara <a.mode.night.show@gmail.com>
2024-05-15 15:47:36 +0900

    pokemon画像をビューに表示

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -12,8 +12,18 @@ class PokemonController extends Controller
         $client = new Client();
         $response = $client->get('https://pokeapi.co/api/v2/pokemon?limit=151');
         $pokemons = json_decode($response->getBody()->getContents(), true);
-        $pokemons = $pokemons['results'];
 
-        return view('pokedex', compact('pokemons'));
+        $detailedPokemons = [];
+        foreach ($pokemons['results'] as $index => $pokemon) {
+            $pokemonDetails = $client->get($pokemon['url']);
+            $pokemonDetails = json_decode($pokemonDetails->getBody()->getContents(), true);
+            $detailedPokemons[] = [
+                'number' => $index + 1,
+                'name' => $pokemon['name'],
+                'image' => $pokemonDetails['sprites']['front_default'],
+            ];
+        }
+
+        return view('pokedex', compact('detailedPokemons'));
     }
 }
--- resources/views/pokedex.blade.php
+++ resources/views/pokedex.blade.php
@@ -5,15 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pokedex</title>
+    <style>
+        .card {
+            border: 1px solid #ccc;
+            border-radius: 10px;
+            padding: 10px;
+            margin: 10px;
+            text-align: center;
+            width: 150px;
+        }
+        .container {
+            display: flex;
+            flex-wrap: wrap;
+        }
+    </style>
 </head>
 <body>
     <h1>Pokedex</h1>
-    <div>
-        <ul>
-            @foreach ($pokemons as $id => $pokemon)
-                <li>No.{{$id + 1}}&nbsp;&nbsp;<a href="">{{ $pokemon['name'] }}</a></li>
-            @endforeach
-        </ul>
+    <div class="container">
+        @foreach ($detailedPokemons as $pokemon)
+        <div class="card">
+            <h2>No. {{$pokemon['number']}} {{ $pokemon['name'] }}</h2>
+            <a href="">
+                <img src="{{ $pokemon['image'] }}" alt="{{ $pokemon['name'] }}">
+            </a>
+        </div>
+        @endforeach
     </div>
 </body>
 </html>
